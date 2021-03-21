c35339d8f7aa56e79135289eb61f23c52cd5f289
Norbert Gogiel <norbert.gogiel@worldpay.com>
2021-03-21 22:47:10 +0000

    Add documentation

--- src/main/java/com/norbertgogiel/LuhnAlgorithms.java
+++ src/main/java/com/norbertgogiel/LuhnAlgorithms.java
@@ -2,12 +2,43 @@ package com.norbertgogiel;
 
 public class LuhnAlgorithms {
 
+    /**
+     * A private constructor.
+     */
     private LuhnAlgorithms() {}
 
+    /**
+     * This method accepts a number as String and checks if it
+     * is valid Luhn number.
+     *
+     * The number has to be numeric and be equal to or smaller
+     * than {@link Long#MAX_VALUE}.
+     *
+     * The method parses the String to long and passes the
+     * value to {@link #calculateLuhnCheck(long)}
+     *
+     * @param number as String
+     * @throws NumberFormatException if the String contains
+     * any other ASCII characters than numbers or if the number
+     * is greater than {@link Long#MAX_VALUE}
+     * @return a boolean indicating if the number is a valid Luhn number
+     */
     public static boolean isValid(String number) {
-        return calculateLuhnCheck(Long.parseLong(number)) % 10 == 0;
+        return isValid(Long.parseLong(number));
     }
 
+    /**
+     * This method accepts a number as long checks if it
+     * is valid Luhn number.
+     *
+     * The number has to be numeric and be equal to or smaller
+     * than {@link Long#MAX_VALUE}.
+     *
+     * The method passes the value to {@link #calculateLuhnCheck(long)}
+     *
+     * @param number as long
+     * @return a boolean indicating if the number is a valid Luhn number
+     */
     public static boolean isValid(long number) {
         return calculateLuhnCheck(number) % 10 == 0;
     }
