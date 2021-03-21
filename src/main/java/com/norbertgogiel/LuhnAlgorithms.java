39dc4c7ca3a252fabdd52cbe4b454dae3ee9620d
Norbert Gogiel <norbert.gogiel@worldpay.com>
2021-03-21 20:45:09 +0000

    Add luhn validation for String

--- src/main/java/com/norbertgogiel/LuhnAlgorithms.java
+++ src/main/java/com/norbertgogiel/LuhnAlgorithms.java
@@ -2,7 +2,15 @@ package com.norbertgogiel;
 
 public class LuhnAlgorithms {
 
+    public static boolean isValid(String number) {
+        return calculateLuhnCheck(Long.parseLong(number)) % 10 == 0;
+    }
+
     public static boolean isValid(long number) {
+        return calculateLuhnCheck(number) % 10 == 0;
+    }
+
+    private static long calculateLuhnCheck(long number) {
         long sum = 0;
         long multiplier = 2;
         do {
@@ -12,6 +20,6 @@ public class LuhnAlgorithms {
             sum += product > 9 ? product - 9 : product;
             multiplier = 3 - multiplier;
         } while (number > 0);
-        return sum % 10 == 0;
+        return sum;
     }
 }
--- src/test/java/com.norbertgogiel/TestLuhnAlgorithms.java
+++ src/test/java/com.norbertgogiel/TestLuhnAlgorithms.java
@@ -6,7 +6,12 @@ import org.junit.jupiter.api.Assertions;
 public class TestLuhnAlgorithms {
 
     @Test
-    public void testIsValid() {
+    public void testLongIsValid() {
         Assertions.assertEquals(true, LuhnAlgorithms.isValid(4444333322221111L));
+    }
+
+    @Test
+    public void testStringIsValid() {
+        Assertions.assertEquals(true, LuhnAlgorithms.isValid("4444333322221111"));
     }
 }
